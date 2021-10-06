99ffb54d97c1446869a6699be0285537e48dc0a5
mado <madelineadonley@gmail.com>
2021-10-06 10:28:24 -0600

    skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// to do
+// fetch api and specify parameters for each of the following instances
+// dynaically create buttons when search bar is clicked and contains a valid city
+// when search bar is clicked, add recent searches by storing in local storage
+// obtain current parameters for city including name, date, icon of weather, humidity, wind UV index, and temp
+// obtain 5 day forecast parameters that displays date, icon of weather, temp, wind, and humidity
+// create if statements for uv index color change to indicate favorable, moderate, or severe conditons
+
+//create variables
+var search = document.getElementById("search-button");
+var currentWeather = document.getElementById("current-weather");
+var forecast = document.getElementById("forecast");
+var recentSearch = document.getElementById("recent-seaarches")
