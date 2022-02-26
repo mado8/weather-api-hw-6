37e95d5726ab4d6f100e79d9407a118c982375fe
mado <madelineadonley@gmail.com>
2022-02-26 15:02:59 -0700

    added recent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
-// to do
-// fetch api and specify parameters for each of the following instances
-// dynaically create buttons when search bar is clicked and contains a valid city
-// when search bar is clicked, add recent searches by storing in local storage
-// obtain current parameters for city including name, date, icon of weather, humidity, wind UV index, and temp
-// obtain 5 day forecast parameters that displays date, icon of weather, temp, wind, and humidity
-// create if statements for uv index color change to indicate favorable, moderate, or severe conditons
-
-//create variables
 const apiKey = `7b2108bf7e20dd2cb90cba345009dacd`
 var searchInput = document.getElementById("search-input");
 var search = document.getElementById("search-button");
 var currentWeather = document.getElementById("current-weather");
 var forecast = document.getElementById("forecast");
-var recentSearch = document.getElementById("recent-seaarches");
+var recentSearch = document.getElementById("recent-searches");
+var myCities = JSON.parse(localStorage.getItem("myCities") || "[]");
 
 var city;
 
@@ -24,6 +16,9 @@ const handleInput = (e) => {
 
 const handleSubmit = (e) => {
     e.preventDefault();
+    myCities.push(city);
+    localStorage.setItem('myCities', JSON.stringify(myCities))
+    console.log(myCities)
     getSearch()
 }
 
@@ -38,5 +33,17 @@ const getSearch = () => {
     })
 }
 
+const renderCities = () => {
+    console.log(myCities)
+    myCities.forEach(city => {
+        console.log(city)
+        let a = document.createElement("a");
+        a.className += "card-body"
+        a.innerHTML = city
+        recentSearch.append(a)
+    })
+}
+
 searchInput.addEventListener("change", handleInput)
 search.addEventListener('click', handleSubmit)
+renderCities();
